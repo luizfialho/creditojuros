2bb405df463aa09d67556c50ec03d0e13055c846
Luiz Henrique Stein Fialho <lhfialho@br.ibm.com>
2018-06-15 01:01:06 -0300

    'alteracoes'

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# creditojuros
+# Juros de Credito
+
+Aplicação rodando em um servidor GlassFish: http://159.203.123.149:8080/creditojuros/
--- src/main/java/com/mycompany/creditojuros/util/ConstantUtils.java
+++ src/main/java/com/mycompany/creditojuros/util/ConstantUtils.java
@@ -5,7 +5,7 @@ public class ConstantUtils {
     public static final String A = "A";
     public static final String B = "B";
     public static final String C = "C";
-    public static final String TEN_PERCENT = "0,1";
-    public static final String TWENTY_PERCENT = "0,2";
+    public static final String TEN_PERCENT = "0.1";
+    public static final String TWENTY_PERCENT = "0.2";
     
 }
